9905f3d9792626ccca834cffb8f849e2ecda9f20
unknown <smkreter@gmail.com>
2020-08-02 13:49:58 -0500

    followed the instructions, json satellite data imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,9 +19,13 @@ export class AppComponent {
  
           let fetchedSatellites = data.satellites;
           // TODO: loop over satellites
-          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
- 
+          for (let i = 0; i < fetchedSatellites.length; i ++) {
+           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+         
+           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+           this.sourceList.push(satellite);
+          }
        }.bind(this));
     }.bind(this));
  
