e3097d45126c7ac0be88a797adbbc30a30749919
unknown <smkreter@gmail.com>
2020-08-02 16:26:19 -0500

    display list is working but search isn't

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,12 @@ let sourceList: Satellite[];
 export class AppComponent {
   title = 'shellies-orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
@@ -26,10 +29,29 @@ export class AppComponent {
            // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
            this.sourceList.push(satellite);
           }
+                // make a copy of the sourceList to be shown to the user
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
- 
+
  }
+
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
+
+
 
 }
 
